908586f6648b363f328f9b43b0974f875215e860
DimitarRadkovDimitrov <ddimitrov360@gmail.com>
2019-04-16 13:41:01 -0400

    Add find and insert BST methods

--- src/binary_tree_node.py
+++ src/binary_tree_node.py
@@ -21,3 +21,32 @@ class Binary_Tree_Node:
             self.post_order_print(node.left_child)
             self.post_order_print(node.right_child)
             print(f"{node.value} ", end="")
+
+    def insert(self, value_to_insert):
+        if value_to_insert < self.value:
+            if self.left_child:
+                self.left_child.insert(value_to_insert)
+            else:
+                self.left_child = Binary_Tree_Node(value_to_insert)
+        elif value_to_insert > self.value:
+            if self.right_child:
+                self.right_child.insert(value_to_insert)
+            else:
+                self.right_child = Binary_Tree_Node(value_to_insert)
+
+    def find(self, value_to_find):
+        if value_to_find < self.value:
+            if self.left_child:
+                return self.left_child.find(value_to_find)
+            else:
+                return False
+        elif value_to_find > self.value:
+            if self.right_child:
+                return self.right_child.find(value_to_find)
+            else:
+                return False
+        else:
+            return True
+
+    def delete(self, value_to_delete):
+        pass
--- test/binary_tree_test.py
+++ test/binary_tree_test.py
@@ -65,3 +65,157 @@ def test_post_order_print_multiple_nodes(capsys):
     expected = "7 9 1 2 6 5 4 "
     output = capsys.readouterr().out
     assert output == expected
+
+def test_insert_one_node(capsys):
+    root_node = Binary_Tree_Node(4)
+    root_node.insert(2)
+    root_node.in_order_print(root_node)
+    
+    expected = "2 4 "
+    output = capsys.readouterr().out
+    assert output == expected
+
+def test_insert_same_node_twice(capsys):
+    root_node = Binary_Tree_Node(4)
+    root_node.insert(2)
+    root_node.insert(2)
+    root_node.in_order_print(root_node)
+
+    expected = "2 4 "
+    output = capsys.readouterr().out
+    assert output == expected
+
+def test_insert_multiple_nodes(capsys):
+    root_node = Binary_Tree_Node(4)
+    root_node.insert(2)
+    root_node.insert(1)
+    root_node.insert(7)
+    root_node.insert(5)
+    root_node.insert(6)
+    root_node.insert(3)
+    root_node.in_order_print(root_node)
+
+    expected = "1 2 3 4 5 6 7 "
+    output = capsys.readouterr().out
+    assert output == expected
+
+def test_insert_as_multiple_nodes_as_linked_list(capsys):
+    root_node = Binary_Tree_Node(1)
+    root_node.insert(3)
+    root_node.insert(5)
+    root_node.insert(7)
+    root_node.in_order_print(root_node)
+
+    expected = "1 3 5 7 "
+    output = capsys.readouterr().out
+    assert output == expected
+
+def test_find_node_in_one_node_tree():
+    root_node = Binary_Tree_Node(7)
+    
+    expected = True
+    output = root_node.find(7)
+    assert output == expected
+
+def test_find_node_not_in_one_node_tree():
+    root_node = Binary_Tree_Node(7)
+    
+    expected = False
+    output = root_node.find(1)
+    assert output == expected
+
+def test_find_node_in_full_tree():
+    root_node = Binary_Tree_Node(4)
+    root_node.insert(2)
+    root_node.insert(1)
+    root_node.insert(7)
+    root_node.insert(5)
+    root_node.insert(6)
+    root_node.insert(3)
+
+    expected = True
+    output = root_node.find(3)
+    assert output == expected
+
+def test_find_node_not_in_full_tree():
+    root_node = Binary_Tree_Node(4)
+    root_node.insert(2)
+    root_node.insert(1)
+    root_node.insert(7)
+    root_node.insert(5)
+    root_node.insert(6)
+
+    expected = False
+    output = root_node.find(3)
+    assert output == expected
+
+def test_find_node_in_linked_list_tree():
+    root_node = Binary_Tree_Node(1)
+    root_node.insert(2)
+    root_node.insert(3)
+    root_node.insert(4)
+    root_node.insert(5)
+
+    expected = True
+    output = root_node.find(5)
+    assert output == expected
+
+def test_delete_node_one_node_tree(capsys):
+    root_node = Binary_Tree_Node(1)
+    root_node.delete(1)
+    root_node.in_order_print(root_node)
+
+    expected = "None "
+    output = capsys.readouterr().out
+    assert output == expected
+
+def test_delete_leaf_node_in_full_tree(capsys):
+    root_node = Binary_Tree_Node(5)
+    root_node.insert(3)
+    root_node.insert(7)
+    root_node.insert(1)
+    root_node.insert(4)
+    root_node.delete(1)
+    root_node.in_order_print(root_node)
+
+    expected = "3 4 5 7 "
+    output = capsys.readouterr().out
+    assert output == expected
+
+def test_delete_node_not_in_full_tree(capsys):
+    root_node = Binary_Tree_Node(5)
+    root_node.insert(3)
+    root_node.insert(7)
+    root_node.insert(1)
+    root_node.insert(4)
+    root_node.delete(12)
+    root_node.in_order_print(root_node)
+
+    expected = "1 3 4 5 7 "
+    output = capsys.readouterr().out
+    assert output == expected
+
+def test_delete_node_with_one_left_child(capsys):
+    root_node = Binary_Tree_Node(5)
+    root_node.insert(3)
+    root_node.insert(7)
+    root_node.insert(1)
+    root_node.delete(3)
+    root_node.in_order_print(root_node)
+
+    expected = "1 5 7 "
+    output = capsys.readouterr().out
+    assert output == expected
+
+def test_delete_node_with_one_right_child(capsys):
+    root_node = Binary_Tree_Node(5)
+    root_node.insert(3)
+    root_node.insert(7)
+    root_node.insert(1)
+    root_node.insert(9)
+    root_node.delete(7)
+    root_node.in_order_print(root_node)
+
+    expected = "1 3 5 9 "
+    output = capsys.readouterr().out
+    assert output == expected
